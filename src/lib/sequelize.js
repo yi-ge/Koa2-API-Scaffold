dd19e64091091bf027e0686ca1304301a26e935d
轶哥 <a@wyr.me>
2017-03-12 20:12:44 +0800

    fix a info

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -4,7 +4,7 @@ import { DB as DBConfig, System as SystemConfig } from '../config'
 export default new Sequelize(DBConfig.database, DBConfig.username, DBConfig.password, {
   host: DBConfig.host,
   dialect: SystemConfig.db_type,
-  dialectOptions: { //MySQL > 5.5，其它数据库删除此项
+  dialectOptions: { // MySQL > 5.5，其它数据库删除此项
     charset: 'utf8mb4',
     collate: 'utf8mb4_unicode_520_ci',
     supportBigNumbers: true,
